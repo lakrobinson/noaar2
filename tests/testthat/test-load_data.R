b033508784337b3f3bd8ebff37c09ac585d91e80
lakrobinson <65979428+lakrobinson@users.noreply.github.com>
2020-11-09 18:59:08 -0500

    Update test-load_data.R

--- tests/testthat/test-load_data.R
+++ tests/testthat/test-load_data.R
@@ -3,6 +3,6 @@ library(testthat)
 
 test_that("Data can be pulled in raw form and loaded into memory", {
   df <- load_data()
-  expect_equal(dim(df)[1], 5979)
+  expect_equal(dim(df)[1], 6207)
   expect_true("data.frame" %in% class(df))
 })
